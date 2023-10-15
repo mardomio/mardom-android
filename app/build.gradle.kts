ae7628d9b0d3fd25a1c3ea1af5cc09677355ca9a
Ava Arya <143613665+avaarya@users.noreply.github.com>
2023-10-15 14:24:27 +0100

    Increment version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "io.mardom"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "2023.10.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
